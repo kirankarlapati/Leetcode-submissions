7992f1b9d03f2800f0d13f5a0cada6fd86d4eeb8
KARLAPATI KIRAN <200835111+kirankarlapati@users.noreply.github.com>
2026-08-27 17:10:05 +0530

    Time: 3 ms (58.83%), Space: 48.5 MB (23.87%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.java
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.java
@@ -3,11 +3,9 @@ class Solution {
         int m=mat.length,n=mat[0].length;
         int[] a=new int[m];
         for(int i=0;i<m;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(mat[i][j]!=0) count++;
-            }
-            a[i]=count;
+            // int count=0;
+            for(int j=0;j<n;j++) if(mat[i][j]!=0) a[i]+=mat[i][j];
+            // a[i]=count;
         }
         return a;
     }
