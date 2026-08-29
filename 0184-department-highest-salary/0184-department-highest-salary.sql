7048d14aa6dfb09b8a54a9433a6672ef78675b89
KARLAPATI KIRAN <200835111+kirankarlapati@users.noreply.github.com>
2026-08-29 23:38:10 +0530

    Time: 1698 ms (5%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -2,5 +2,4 @@
 select dpt.name as Department, emp.name as Employee, emp.salary as Salary
 from Employee emp
 join Department dpt
-on emp.departmentId=dpt.id
-having salary=(select max(e.salary) from Employee e where e.departmentId=emp.departmentId);
+on emp.departmentId=dpt.id having salary=(select max(e.salary) from Employee e where e.departmentId=emp.departmentId);
